49719b69fba55b9124eec6a8aebfd74e9e011e42
winechit-nfq <wine.chit@nfq.com>
2025-07-29 14:23:26 +0700

    refactor: reorganize CV review tasks and enhance software engineer reviewer role
    - Moved the software engineer CV review task to a new structure for better clarity and integration.
    - Updated the expected output for generalist, UX designer, and product manager tasks to include more detailed analysis.
    - Added a comprehensive final assessment task for the software engineer role, integrating insights from other reviewers.
    - Improved YAML configurations for agents to ensure consistency and clarity in the review process.

--- backend/crew.py
+++ backend/crew.py
@@ -2,6 +2,7 @@ from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from typing import List
+
 # If you want to run a snippet of code before or after the crew starts,
 # you can use the @before_kickoff and @after_kickoff decorators
 # https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
@@ -56,12 +57,6 @@ class CrewAI():
         )
 
     @task
-    def software_engineer_cv_review_task(self) -> Task:
-        return Task(
-            config=self.tasks_config['software_engineer_cv_review_task'],
-        )
-
-    @task
     def ux_designer_cv_review_task(self) -> Task:
         return Task(
             config=self.tasks_config['ux_designer_cv_review_task'], 
@@ -71,6 +66,13 @@ class CrewAI():
     def product_manager_cv_review_task(self) -> Task:
         return Task(
             config=self.tasks_config['product_manager_cv_review_task'],
+        )
+
+    @task
+    def software_engineer_cv_review_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['software_engineer_cv_review_task'],
+            async_execution=True,
             output_file='report.md'
         )
 
